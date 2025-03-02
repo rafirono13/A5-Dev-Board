50c3d64f233b656f9cc39fcc220b7458b8ecfcc0
rafirono13 <129680992+rafirono13@users.noreply.github.com>
2025-03-02 23:07:30 +0600

    changelog:clear history btn issue fixed

--- JS folder/button.js
+++ JS folder/button.js
@@ -17,13 +17,11 @@ for (let i = 0; i < buttons.length; i++){
         taskDecrementElement.textContent = currentDecrementCount;
         let cardContainer = this.closest('.p-6');
         
-        // Get the task title from within that container
-        let taskTitle = cardContainer.querySelector('.task-title').textContent.trim();
 
-        // Get the current time
+        let taskTitle = cardContainer.querySelector('.task-title').textContent;
+
+
         let currentTime = new Date().toLocaleTimeString();
-
-        // Create the new activity log div
         let activityDiv = document.createElement('div');
         activityDiv.className = "bg-slate-300 w-80 h-auto mx-auto rounded-lg p-4 space-y-2 mb-4";
         activityDiv.textContent = `You have completed "${taskTitle}" at ${currentTime}`;
@@ -31,13 +29,18 @@ for (let i = 0; i < buttons.length; i++){
         // Append it to the activity log container
         activityLogContainer.appendChild(activityDiv);
 
+        
 
     });
 }
 
 
-// buttons.forEach(function (button) {
-//     button.addEventListener('click', function(){
-//         this.disabled = true;
-//     })
-// })
+let clearHistoryButton = document.getElementById('clear-history-btn');
+
+if (clearHistoryButton) {
+    clearHistoryButton.addEventListener('click', function () {
+      
+        let activityLogContainer = document.getElementById('msg-container');
+        activityLogContainer.innerHTML = '';
+    });
+}
